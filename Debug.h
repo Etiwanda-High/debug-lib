378fefd18617328a765743eb632bcc57166bc3c8
KingRainbow44 <kobedo11@gmail.com>
2025-04-17 10:08:39 -0700

    feat: add debug print

--- Debug.h
+++ Debug.h
@@ -4,11 +4,17 @@
 
 #if DEBUG
 
-#define DEBUG_LED 3
+#include <Arduino.h>
+
+#define PRINT(...) Serial.println(__VA_ARGS__)
+
+#define DEBUG_LED 12
 #define BAUD_RATE 9600
 
 /**
  * Invoked once when the Arduino is initialized.
  */
 void debug_setup();
+#else
+#define PRINT(...)
 #endif
